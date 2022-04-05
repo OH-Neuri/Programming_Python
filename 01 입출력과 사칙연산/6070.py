ac27ed05c05469c90a0335f4bfba380b48356501
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-04-06 00:14:50 +0900

    [CodeUp] [기초-조건/선택실행구조] 월 입력받아 계절 출력하기

---
+++ 01 입출력과 사칙연산/6070.py
@@ -0,0 +1,9 @@
+a= int(input()) # 0이 아니면 입력된 정수를 출력하고, 0이 입력되면 출력을 중단해보자.
+if a//3==1 :
+    print("spring")
+elif a//3==2:
+    print("summer")
+elif a//3==3:
+    print("fall")
+else:
+    print("winter")
