511764033073eefa1353ae55d3f4e38390dad18d
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-04-11 20:23:55 +0900

    [CodeUp] [기초-리스트] 바둑판에 십자 뒤집기

---
+++ 01 입출력과 사칙연산/6096.py
@@ -0,0 +1,19 @@
+d=[list(map(int, input().split())) for i in range(19)]
+
+n=int(input())
+for i in range(n):
+    x, y = input().split()
+    for j in range(19):
+        if d[int(x)-1][j]==0:
+            d[int(x)-1][j]=1
+        else:
+            d[int(x)-1][j]=0
+
+        if d[j][int(y)-1]==0:
+            d[j][int(y)-1]=1
+        else:
+            d[j][int(y)-1]=0
+for i in range(19):
+    for j in range(19):
+        print(d[i][j], end =' ')
+    print()
