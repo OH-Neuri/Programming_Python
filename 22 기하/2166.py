8f81408275c5469a61691c187618a92ef5307d9d
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-04-29 21:33:29 +0900

    [Baekjoon] [기하] 다각형의 면적

--- 22 기하/2166.py
+++ 22 기하/2166.py
@@ -1,17 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-N=int(input())
-list=[]
 
-for i in range(N):
-    x, y =map(int,input().split())
-    list.append([x, y])
 
-list.sort(key=lambda x: x[0])
-x1= list[N-1][0]-list[0][0]
 
-list.sort(key=lambda x: x[1])
-y1= list[N-1][1]-list[0][1]
-
-print(f'{x1*y1:.1f}')
