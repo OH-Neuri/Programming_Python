433cae89b9ab337c34da84f23802148371c502b8
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-05-02 23:11:57 +0900

    [이취테] [그리디] 큰 수의 법칙2

--- 14 그리디/큰 수의 법칙2.py
+++ 14 그리디/큰 수의 법칙2.py
@@ -1,4 +1,16 @@
 n, m, k = map(int, input().split()) # 5 8 3      2 4 5 4 6
-sum=0
-cnt=0
+data = list(map(int,input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+count = int(m/(k+1)*k)
+count+= m%(k+1)
+
+result = 0
+result += (count)*first
+result += (m-count)* second
+
+print(result)
 
