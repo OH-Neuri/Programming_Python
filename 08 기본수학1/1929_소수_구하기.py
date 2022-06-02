15007a18e84d712ac6e1b5e407ec0a4a430ca635
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-06-02 22:49:46 +0900

    [Baekjoon] [기본수학1] 소수 구하기

---
+++ 08 기본수학1/1929_소수_구하기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+M, N= map(int,input().split())
+
+for i in range(M,N):
+    for j in range(2,N):
+        if i%j==0:
+            if i==j:
+                print(i)
+                break
+            break
