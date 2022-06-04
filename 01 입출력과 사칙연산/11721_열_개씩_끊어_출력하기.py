e58000e3648ba15856a62924b016e4464b4b8512
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-06-04 23:45:00 +0900

    [Baekjoon] [입출력과 사칙연산] 열 개씩 끊어 출력하기

--- 01 입출력과 사칙연산/11721_열_개씩_끊어_출력하기.py
+++ 01 입출력과 사칙연산/11721_열_개씩_끊어_출력하기.py
@@ -1,9 +1,4 @@
-from collections import deque
-
-text=deque(input())
-while len(text)>0:
-    for i in range(10):
-        print(text.popleft(),end='')
-        if len(text)==0:
-            break
-    print()
+a = input()
+for i in range(0,len(a),10):
+    b=a[i:i+10]
+    print(b)
