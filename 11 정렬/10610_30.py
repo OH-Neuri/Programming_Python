cc0e969c6cb5f88bf55129ab4d0694e9a8d5436e
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-06-12 22:51:29 +0900

    [Baekjoon] [정렬] 30

---
+++ 11 정렬/10610_30.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+
+N=(input())
+sum=0
+if N.find('0')!=-1:
+    for i in range(len(N)):
+        sum+=(N[i])
+    if sum%3==0:
+        N.sort(reverse=True)
+    print(N)
+else:
+    print('-1')
+
+    ## 아직 덜했음 ## 아직 덜했음 ## 아직 덜했음 ## 아직 덜했음
