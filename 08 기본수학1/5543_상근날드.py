c3931570fc663884e204e6647bb95d3a3fb124ae
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-07-24 23:18:42 +0900

    [Baekjoon] [기본수학1] 상근날드

--- 08 기본수학1/5543_상근날드.py
+++ 08 기본수학1/5543_상근날드.py
@@ -1,1 +1,9 @@
-for i in range(5):
+hamPrice = []
+coPrice = []
+for i in range(3):
+    hamPrice.append(int(input()))
+for i in range(2):
+    coPrice.append(int(input()))
+hamPrice.sort()
+coPrice.sort()
+print((hamPrice[0]+coPrice[0])-50)
