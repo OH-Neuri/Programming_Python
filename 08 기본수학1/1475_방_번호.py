eef027406e09a4e99cfa6aac077d1204bc6240c8
OH-Neuri <87141845+OH-Neuri@users.noreply.github.com>
2022-07-29 23:50:31 +0900

    [Baekjoon] [기본수학1] 방 번호

--- 08 기본수학1/1475_방_번호.py
+++ 08 기본수학1/1475_방_번호.py
@@ -1,18 +1,16 @@
 N=input()
 M=int(N)
-
 Num=[0 for _ in range(10)]
 
 for i in range(len(N)):
-    Num[M%10]+=1;
+    if M%10==9:
+        Num[6]+=1;
+    else:
+        Num[M%10]+=1;
     M=M//10
+if (Num[6]/2)%2==0:
+    Num[6] = Num[6]/2
+else:
+    Num[6] = (Num[6] / 2)+1
 
-if Num[6]>=2:
-    Num[6]= int(Num[6]/2)
-if Num[9]>=2:
-    Num[9] = int(Num[9]/2)
-
-Num[6] = Num[6]+Num[9]
-Num[9] = Num[6]
-
-print(max(Num))
+print(int(max(Num)))
